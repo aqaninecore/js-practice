3e802cfe5e27fb335f07fb5266805eb11d8e17da
aqanincore <yuri.sekachev@gmail.com>
2025-12-09 18:46:02 +0100

    updates for gitignore.file

---
+++ lesson-4/numbers.js
@@ -0,0 +1,39 @@
+// const x = 0.1
+// const y = 0.2
+// const sum = x + y
+// console.log(x + y)
+
+// console.log(0.17 + 0.24)
+// console.log(0.17 * 100 + 0.24 * 100)
+
+// console.log(0.17 + 0.24)
+// console.log((0.17 + 0.24).toFixed(2))
+
+
+// console.log((Math.PI).toFixed(5)) // lib Math
+// console.log(Math.pow(3, 4)) // lib Math (stepen')
+
+
+// let x = 5.99
+// let rounded = Math.round(x)
+// console.log(rounded)
+
+// let x = 7.99
+// let rounded = Math.round(x)
+// console.log(rounded)
+
+// let y = 45.49
+// let roundedFloor = Math.floor(y)
+// console.log(roundedFloor)
+
+// let z = 151.01
+// let roundedCeil = Math.ceil(z)
+// console.log(roundedCeil)
+
+
+// const number = 4.687
+// const roundedString = number.toFixed(2) // "4.69"
+// console.log(roundedString)
+
+// console.log(Math.random() * 10)
+// console.log(Math.floor(Math.random() * 11))
---
+++ lesson-4/strings.js
@@ -0,0 +1,36 @@
+// let string1 = "Hello"
+// let string2 = 'World'
+// let string3 = `!`
+
+// let emre = "Yuriy"
+// let concatString = 'My name is: ' + emre + ', I am from Mars'
+// let concatString2 = `My name is %{emre}, I am from Mars` // template string
+
+// console.log(concatString2)
+
+
+let testString = "Hello World"
+let testString2 = 'Hello Saturn'
+// console.log(testString[10])
+// console.log(testString.length)
+// console.log(testString[testString.length - 1])
+
+// testString = 'New String'
+
+// console.log(testString.chartAt(0))
+// console.log(testString.indexOf('Wo'))
+// console.log(testString.concat(testString2))
+// console.log(testString.lastIndexOf('l'))
+// console.log(testString.substring(3))
+//console.log(testString.toLowerCase())
+//console.log(testString.toUpperCase())
+
+// console.log(testString.replace('l', '9'))
+// console.log(testString.replaceAll('l', '9'))
+
+// console.log(testString.split(','))
+
+// console.log(testString.startsWith('H'))
+// console.log(testString2.endsWith('n'))
+
+// console.log(testString2.includes('Saturn'))
