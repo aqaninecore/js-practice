cb7b40eb90409c6af33a51ff10bf16916c4b97fb
aqanincore <yuri.sekachev@gmail.com>
2025-12-09 18:58:30 +0100

    Restore lesson-4 files: operator, strings, types from previous commit

---
+++ lesson-4/operator.js
@@ -0,0 +1,82 @@
+
+// let sum = 5 + 3
+// console.log(sum)
+
+
+// let result = 10 - 5
+// console.log(result)
+
+
+// let result = 10 * 5
+// console.log(result)
+
+
+// let result = 18 / 5
+// console.log(result)
+
+
+// let result = (10 + 5) * 20
+// console.log(result)
+
+
+// let result = 10 % 6
+// console.log(result)
+
+
+// let number = 12
+// console.log(number % 2)
+
+
+// +=
+// let x = 5
+// x += 3 // x = x + 3
+// console.log(x)
+
+
+// // -=
+// let y = 10
+// y -= 4 // y = y - 4
+// console.log(y)
+
+
+// let x = 10
+// x *= 3
+// console.log(x)
+
+
+// let a = 10
+// a++
+// a = a + 1
+// a += 1
+// console.log(a)
+
+
+// //a--
+// a = a - 1
+// a -= 1
+// console.log(a)
+
+
+// let a = 5
+// let b = 10
+// console.log(a)
+// console.log(b)
+
+
+// let a = 5
+// let b = a++  // b = 5, a = 6
+// console.log(a)
+// console.log(b)
+
+
+// console.log(5 == '5')
+// console.log(5 == 5)
+
+
+// console.log(10 !== '10')
+
+
+// console.log(5 >= 10)
+
+
+// console.log(1 == true)
---
+++ lesson-4/types.js
@@ -0,0 +1,36 @@
+
+// // String -> Number
+
+// // console.log(parseInt('55')) // number
+// // console.log('55') // string
+// // console.log(Number('102'))
+// // console.log(+('102')) // number also
+// // console.log(parseFloat('5.99'))
+
+
+// // Number -> String
+
+// let numberA = 55
+// let numberB = 45
+// let numberC = 11.99
+
+// console.log(String(numberA))
+// console.log(numberB.toString())
+// console.log(numberC.toString())
+
+
+// // *** -> Boolean
+
+// console.log(Boolean(1)) // true
+// console.log(Boolean(2)) // true
+// console.log(Boolean(-154)) // true
+// console.log(Boolean(0)) // false
+
+// console.log(Boolean('String'))
+// console.log((''))
+// console.log(Boolean('4251'))
+
+// console.log('test' + 10)
+// console.log('1' + 10)
+// console.log(5 * 10)
+// console.log(true * 10)
