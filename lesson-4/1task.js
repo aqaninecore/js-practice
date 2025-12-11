da70e4f40ad50d096e67b4c94ef2229ce17cb4aa
aqanincore <yuri.sekachev@gmail.com>
2025-12-11 18:11:43 +0100

    execute 1task

---
+++ lesson-4/1task.js
@@ -0,0 +1,11 @@
+import chalk from "chalk"
+
+// String concatenation and template literals
+let user = 'User1'
+let welcome = "Hello"
+console.log(chalk.gray(welcome , user))
+welcome = 'Hi'
+console.log(`${welcome}, ${user}!`)
+
+
+// Number operations
