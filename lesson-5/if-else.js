59706a9f59df1433d37d7b9a12a85d60adce1769
aqanincore <yuri.sekachev@gmail.com>
2025-12-11 20:12:32 +0100

    Implement 1task: done

---
+++ lesson-5/if-else.js
@@ -0,0 +1,136 @@
+// let x =3
+
+// if (x >= 5) {
+//     console.log("x is greater than 5")
+// }
+
+// if (x === 5) {
+//     console.log("x is equal to 5")
+// }
+
+
+// if (x > 5) {
+//     console.log("x is greater than 5")
+//     if (x < 50) {
+//         console.log("x is also less than 50")
+//     }
+// }
+
+// let time = 2
+
+// if (time < 5) {
+//     console.log("Good night")
+// } else if (time < 10) {
+//     console.log("Good morning")
+// } else if (time < 18) {
+//     console.log("Good day")
+// } else if (time < 24) {
+//     console.log("Good evening")
+// }
+
+// let time = 22234
+
+// if (time < 5) {
+//     console.log("Good night")
+// } else if (time < 10) {
+//     console.log("Good morning")
+// } else if (time < 18) {
+//     console.log("Good day")
+// } else if (time < 24) {
+//     console.log("Good evening")
+// } else {
+//     console.log("Invalid time")
+// }
+
+// let x = 10
+// if (x > 5) {
+//     console.log("x is greater than 5")
+// } else {
+//     console.log("x is less or equal to 5")
+// }
+
+// console.log(!true)
+// console.log(!5)
+
+// let isAdult = true
+
+// if (isAdult === true) {
+//     console.log("You can buy candy")
+// }
+
+// if (isAdult) {
+//     console.log("You can buy candy")
+// }
+
+// let isAdult = false
+// if (!isAdult) {
+//     console.log("You can buy candy")
+// }
+
+
+// let isSleep = false
+
+// if (!isSleep) {
+//     console.log("Person not sleeping!")
+// }
+
+
+// console.log(!!true)
+// console.log(!!0)
+// console.log(!!1)
+// console.log(!!"Hello")
+// console.log(!!"")
+// console.log(!!null)
+// console.log(!!undefined)
+// console.log(!!NaN)
+
+
+// const digitA = -15
+// const digitB = -10
+
+// if (digitA > 0 && digitB > 0) {
+//     console.log("Both numbers are positive")
+// } else if (digitA > 0 && digitB < 0) {
+//     console.log("Both numbers are negative")
+// }
+// else {
+//     console.log("At least one number is not positive")
+// }
+
+
+
+// let age = 60
+
+// if (age >= 3 && age <= 13) {
+//     console.log("You are a child")
+// } else if (age >= 14 && age <= 18) {
+//     console.log("You are a teenager")
+// } else if (age >= 19 && age <= 30) {
+//     console.log("You are a young adult")
+// } else if (age >= 31 && age <= 99) {
+//     console.log("You are an adult")
+// } else if(age <= 0) {
+//     console.log("Invalid age")
+// }
+// else {
+//     console.log("You are too young/adult")
+// }
+
+
+// const digitA = 4
+// const digitB = -5
+
+// if (digitA > 0 && digitB > 0) {
+//     console.log("Both numbers are positive")
+// } else if (digitA < 0 || digitB < 0) {
+//     console.log("At least one number is negative")
+// }
+
+// else {
+//     console.log("At least one number is not positive")
+// }
+
+
+let age = 17
+let message = age >=18 ? "Access granted" : "Access denied" // check  true/false
+console.log(message)
---
+++ lesson-5/switch-case.js
@@ -0,0 +1,45 @@
+// let fruit = 'apple'
+
+// switch (fruit) {
+//     case 'banana':
+//         console.log("It's a banana")
+//         break
+//     case 'apple':
+//         console.log("It's an apple")
+//         break
+//     case 'orange':
+//         console.log("It's an orange")
+//         break
+//     default:
+//         console.log("Unknown fruit")
+// }
+
+
+// let fruit = 'potato'
+
+// switch (fruit) {
+//     case 'banana':
+//         console.log("It's a banana")
+//         break
+//     case 'apple':
+//         console.log("It's an apple")
+//         break
+//     case 'orange':
+//         console.log("It's an orange")
+//         break
+//     default:
+//         console.log("Unknown fruit")
+// }
+
+
+let digit = 60
+switch (true) {
+    case digit > 50:
+        console.log(">50")
+        break
+    case digit > 20:
+        console.log(">20")
+        break
+    default:
+        console.log("<=20")
+}
