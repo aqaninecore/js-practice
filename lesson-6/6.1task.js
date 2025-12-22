3ab4debd3b8514eb49112d524c9b4a2e85e43fb2
aqanincore <yuri.sekachev@gmail.com>
2025-12-22 03:26:23 +0100

    homework. lesson 6

---
+++ lesson-6/6.1task.js
@@ -0,0 +1,32 @@
+import chalk from 'chalk'
+
+
+// FUNCTION DECLARATION
+
+function rectangleArea(width, height) {
+    const value = width * height
+    return value
+}
+
+const area = rectangleArea(7, 9)
+console.log(chalk.bgBlack(chalk.yellow(area)))
+
+//////////////////////////////////////////////////////////////
+// FUNCTION EXPRESSION
+
+const rectArea = function (a, b) {
+    const multiply = a * b
+    return multiply
+}
+
+const area1 = rectArea(7, 8)
+console.log(chalk.bgBlack(chalk.red(area1)))
+
+//////////////////////////////////////////////////////////////
+// ARROW FUNCTION EXPRESSION
+const reArea = (i, j) => {
+    return i * j
+}
+
+const area2 = reArea(8, 9)
+console.log(chalk.bgBlack(chalk.cyan(area2)))
---
+++ lesson-6/6.3task.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk'
+
+
+let checkOrder = (ordered, available) => {
+    if (ordered > available) {
+        console.log(chalk.red.bold('Your order is too large, we don’t have enough goods.'))
+    }
+    if (ordered <= 0) {
+        console.log(chalk.yellow.bgCyan('Your order is empty'))
+    }
+    else {
+        console.log(chalk.green.italic.bold('Your order is accepted'))
+    }
+}
+
+checkOrder(100, 99)
+checkOrder(1001, 1001)
+checkOrder(0, 1)
+checkOrder(-1, 0)
