376f05679a58d10db1052dd7f72f5da65041e9ff
aqanincore <yuri.sekachev@gmail.com>
2026-01-07 22:09:22 +0100

    Homework for lesson 7

---
+++ lesson-7/7.1task.js
@@ -0,0 +1,21 @@
+import chalk from 'chalk'
+
+
+const handleNum = (num, evenCallback, oddCallback) => {
+    if (num % 2 === 0) {
+        evenCallback()
+    } else {
+        oddCallback()
+    }
+}
+
+const handleEven = () => {
+    console.log(chalk.magentaBright('number is even'))
+}
+
+const handleOdd = () => {
+    console.log(chalk.cyanBright('number is odd'))
+}
+
+handleNum(15, handleEven, handleOdd)
+handleNum(20, handleEven, handleOdd)
